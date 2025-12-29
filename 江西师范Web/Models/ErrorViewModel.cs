acf1d76296e083bcf770abea578246bb0fbfffa4
1172058675@qq.com <1172058675@qq.com>
2025-12-29 17:33:50 +0800

    添加 MVC 项目初始文件

---
+++ 江西师范Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace 江西师范Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
