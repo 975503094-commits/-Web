e802d7d1118a800e618bc889769c788ebb35caf2
975503094-commits <975503094@qq.com>
2025-12-29 19:51:11 +0800

    Delete 江西师范Web directory

--- 江西师范Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace 江西师范Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
